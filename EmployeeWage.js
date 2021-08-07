37c9ac969a4ed910faf49336f6957acfa20f829e
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-07 22:29:27 +0530

    Added:To calculate the Wage for an Employee using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,18 +4,23 @@ const parttimeHrs = 4;
 const fulltimeHrs = 8;
 const wagePerHr = 20;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case isparttime:
-        empHrs = parttimeHrs;
-        break;
-    case isfulltime:
-        empHrs = fulltimeHrs;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+
+    switch (empCheck) {
+        case 1:
+            return parttimeHrs;
+            break;
+        case 2:
+            return fulltimeHrs;
+            break;
+        default:
+            return 0;
+    }
 }
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 
 let empWage = empHrs * wagePerHr;
 console.log("Employee Wage: " + empWage);
