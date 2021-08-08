1ab3ede9395ad633bfdafbcaee95d2b6715b48ec
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-08 21:10:31 +0530

    Added:To calculate the Wage and use Arrow functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -108,3 +108,30 @@ console.log("Number of Days Employee Worked: " + empDailyWageArr.reduce(totalDay
 
 console.log(mapDaywithWageArr);
 console.log("Employee wage Map totalHrs: " + Array.from(mapDaywithWageArr.values()).reduce(totalWages, 0));
+
+//Use Arrow Functions to calculate the wage
+
+const findTotal = (totalVal, dailyVal) => {
+    return totalVal + dailyVal;
+}
+
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal, 0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal, 0);
+
+console.log("TotalHours: " + totalHours);
+console.log("Total Wages: " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empDailyHrsMap.forEach((value, key) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log("Full Working Days: " + fullWorkingDays);
+console.log("Part Working Days: " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
