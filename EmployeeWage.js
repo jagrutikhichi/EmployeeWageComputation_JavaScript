e592fc456f3d4adc3e7ec7b032ae6bf4485e2f07
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-08 20:48:39 +0530

    Added:To calculate the Wage and store it

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -18,18 +18,30 @@ function getWorkingHours(empCheck) {
     }
 }
 
-const maxHrsinMonth = 120;
+//To calculate the Wage of the Employee
+function calcDailyWage(empHrs) {
+    return empHrs * wagePerHr;
+}
+
+const maxHrsinMonth = 160;
 const numofWorkingDays = 20;
 let totalempHrs = 0;
 let totalWorkingdays = 0;
 let empHrs = 0;
+let empDailyWageArr = new Array();
+
+//To check Maximum Working Hours and Number of Working Days Conditions
 while (totalempHrs <= maxHrsinMonth && totalWorkingdays < numofWorkingDays) {
     totalWorkingdays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    empHrs = getWorkingHours(empCheck);
+    totalempHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
 }
 
-let empWage = empHrs * wagePerHr;
+let empWage = calcDailyWage(totalempHrs);
+
 console.log("Total Days: " + totalWorkingdays);
-console.log("Total Hours: " + empHrs);
+console.log("Total Hours: " + totalempHrs);
+console.log("Daily Wage Array: " + empDailyWageArr);
 console.log("Employee Wage: " + empWage);
